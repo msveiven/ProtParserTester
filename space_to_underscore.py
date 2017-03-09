48880648463c2f8998972758bd0c1fd972f04df5
msveiven <sveiven@email.arizona.edu>
2017-03-09 11:08:28 -0700

    Delete space_to_underscore.py

--- space_to_underscore.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import os
-
-def spaceToUnderscore():
-    for filename in os.listdir("."):
-        os.rename(filename, filename.replace(" ", "_"))
-        #print('in space to underscore')
-    return
